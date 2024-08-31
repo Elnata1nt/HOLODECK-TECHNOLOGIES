f2461f9c6e5132ff7630eebdc6c077d9815f9f42
Elnata Correa <elnata_correa@hotmail.com>
2024-08-30 21:51:55 -0400

    alteraçao
    adicionei um gig

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
+
+// import React from "react";
+// import AppRouter from "./router";
+// import Header from "./components/Layout/Header";
+// import Footer from "./components/Layout/Footer";
+// import '@/styles/index.css';
+
+// const App = () => {
+//   return (
+//     <div className="flex flex-col min-h-screen relative">
+//       <div className="flex-grow relative z-10">
+//       <Header />
+//         <AppRouter />
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
 // src/App.jsx
 import React from "react";
 import AppRouter from "./router";
 import Header from "./components/Layout/Header";
 import Footer from "./components/Layout/Footer";
+import { AuthProvider } from './contexts/AuthContext'; // Certifique-se de ajustar o caminho, se necessário.
 import '@/styles/index.css';
 
 const App = () => {
   return (
-    <div className="flex flex-col min-h-screen relative">
-      <div className="flex-grow relative z-10">
-      <Header />
-        <AppRouter />
+    <AuthProvider>
+      <div className="flex flex-col min-h-screen relative">
+        <div className="flex-grow relative z-10">
+          <Header />
+          <AppRouter />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </AuthProvider>
   );
 };
 
